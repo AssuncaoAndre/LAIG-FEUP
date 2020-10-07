9ed8ff15e177a6cd9a7796bb56e56066cca5e386
Luís Assunção <up201806140@fe.up.pt>
2020-10-07 12:22:17 +0100

    added sphere and triangle

--- TP1/MyCylinder.js
+++ TP1/MyCylinder.js
@@ -1,7 +1,11 @@
 class MyCylinder extends CGFobject {
-    constructor(scene, bottomRadius, topRadius, height,  slices, stacks) {
+    constructor(scene,height,bottomRadius,topRadius, stacks, slices) {
         super(scene);
         this.slices = slices;
+        this.stacks=stacks;
+        this.height=height;
+        this.bottomRadius=bottomRadius;
+        this.topRadius=topRadius;
         this.initBuffers();
     }
     initBuffers() {
@@ -10,23 +14,25 @@ class MyCylinder extends CGFobject {
         this.normals = [];
         this.texCoords = [];
 
-        var ang = 0;
-        var alphaAng = 2 * Math.PI / this.slices;
+        var angb = 0;
+        var angt=0;
+        var alphaAngb = 2 * Math.PI*this.bottomRadius / this.slices;
+        var alphaAngt = 2 * Math.PI*this.topRadius/ this.slices;
 
         for (var i = 0; i < this.slices; i++) {
             // All vertices have to be declared for a given face
             // even if they are shared with others, as the normals 
             // in each face will be different
 
-            var sa = Math.sin(ang);
-            var saa = Math.sin(ang + alphaAng);
-            var ca = Math.cos(ang);
-            var caa = Math.cos(ang + alphaAng);
+            var sa = Math.sin(angb);
+            var saa = Math.sin(angb + alphaAngb);
+            var ca = Math.cos(angt);
+            var caa = Math.cos(angt + alphaAngt);
 
             this.vertices.push(ca , 0, -sa );
             this.vertices.push(caa , 0, -saa );
-            this.vertices.push(ca , 1, -sa );
-            this.vertices.push(caa , 1, -saa );
+            this.vertices.push(ca , this.height, -sa );
+            this.vertices.push(caa , this.height, -saa );
 
             this.normals.push(ca , 0, -sa );
             this.normals.push(caa , 0, -saa );
@@ -41,7 +47,8 @@ class MyCylinder extends CGFobject {
             this.texCoords.push(1 / this.slices * i, 0);
             this.texCoords.push(1 / this.slices * (i + 1), 0);
 
-            ang += alphaAng;
+            angb+= alphaAngb;
+            angt+= alphaAngt;
         }
 
         this.primitiveType = this.scene.gl.TRIANGLES;
--- TP1/XMLscene.js
+++ TP1/XMLscene.js
@@ -37,6 +37,8 @@ class XMLscene extends CGFscene {
         this.loadingProgress=0;
 
         this.defaultAppearance=new CGFappearance(this);
+        this.cylinder = new MyCylinder(this, 2,1,0,10,10);
+        //this.sphere=new MySphere(this,1.5,10,10);
 
     }
 
@@ -139,6 +141,8 @@ class XMLscene extends CGFscene {
         }
 
         this.popMatrix();
+        //this.sphere.display();
+        this.cylinder.display();
         // ---- END Background, camera and axis setup
     }
 }
